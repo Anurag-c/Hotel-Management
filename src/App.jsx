840bae348feb5a0d126478ccd83ff2d81cd7b49f
Anurag Nampally <anurag.nampally@gmail.com>
2024-05-28 14:10:46 -0400

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+`;
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
