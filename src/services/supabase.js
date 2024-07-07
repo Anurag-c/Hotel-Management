0085bc730171fd382a5a9207de3ae584d3c80ed8
Anurag Nampally <anurag.nampally@gmail.com>
2024-07-07 17:34:27 -0400

    env variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://gdbpdiciwmawtjbktmmo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkYnBkaWNpd21hd3RqYmt0bW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwMDU1MzYsImV4cCI6MjAzMjU4MTUzNn0.F-5G5HTYr2amBN-N3vWqV_7_8ndzYKuF7R8lEXMWBBw";
+export const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
